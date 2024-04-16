394cabe19519406d9b1954a2a643ddaafdd368b2
Sathsara Karunarathne <79186512+RomiS08@users.noreply.github.com>
2024-04-16 11:31:31 +0530

    Debugging mode

--- sendemail.php
+++ sendemail.php
@@ -1,30 +1,26 @@
+<?php
 
-<?php 
+class SendEmail {
 
-    require 'vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
+    public static function SendMail($to, $subject, $content) {
+        // Get the API key from Heroku config vars
+        $apiKey = $_ENV['API_KEY'];
+        $email = new \SendGrid\Mail\Mail();
+        $email->setFrom("shone58deal345@gmail.com", "Sathsara Karunarathne");
+        $email->setSubject($subject);
+        $email->addTo($to);
+        $email->addContent("text/plain", $content);
+        //$email->addContent("text/html", $content);
 
-    class SendEmail{
+        $sendgrid = new \SendGrid($apiKey);
 
-        public static function SendMail($to,$subject,$content){
-            $apiKey = $_ENV['API_KEY'];
-            $email = new \SendGrid\Mail\Mail();
-            $email->setFrom("shone58deal345@gmail.com", "Sathsara Karunarathne");
-            $email->setSubject($subject);
-            $email->addTo($to);
-            $email->addContent("text/plain", $content);
-            //$email->addContent("text/html", $content);
-
-            $sendgrid = new \SendGrid($apiKey);
-
-            try{
-                $response = $sendgrid->send($email);
-                return $response;
-            }catch(Exception $e){
-                echo 'Email exception Caught : '. $e->getMessage() ."\n";
-                return false;
-            }
+        try {
+            $response = $sendgrid->send($email);
+            return $response;
+        } catch (Exception $e) {
+            echo 'Email exception Caught : ' . $e->getMessage() . "\n";
+            return false;
         }
     }
+}
 ?>
