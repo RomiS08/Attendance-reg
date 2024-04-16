f6cf2d8c69f5aa0b74f8feac83116e1b5734d9cc
Sathsara Karunarathne <79186512+RomiS08@users.noreply.github.com>
2024-04-16 14:13:36 +0530

    Bug Fixed And Code Improved with Logically/env loading

--- sendemail.php
+++ sendemail.php
@@ -1,29 +1,35 @@
 <?php
-    require 'vendor/autoload.php';
+
+require 'vendor/autoload.php';
+
+// Load environment variables conditionally
+if (file_exists('.env')) {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
+}
 
 class SendEmail {
-
     public static function SendMail($to, $subject, $content) {
-        // Get the API key from Heroku config vars when Deployed and Run top up the the Heroku
-        $apiKey = $_ENV['API_KEY'];
+        // Access environment variables
+        $apiKey = getenv('API_KEY');
+
         $email = new \SendGrid\Mail\Mail();
         $email->setFrom("shone58deal345@gmail.com", "Sathsara Karunarathne");
         $email->setSubject($subject);
         $email->addTo($to);
         $email->addContent("text/plain", $content);
-        //$email->addContent("text/html", $content);
 
         $sendgrid = new \SendGrid($apiKey);
 
         try {
             $response = $sendgrid->send($email);
             return $response;
-        } catch (Exception $e) {
-            echo 'Email exception Caught : ' . $e->getMessage() . "\n";
+        } catch(Exception $e) {
+            echo 'Email exception Caught : '. $e->getMessage() ."\n";
             return false;
         }
     }
 }
+
+
 ?>
